bbbe43e3e6ed3d3bb9eedded348175ad491a0092
Anna Egorova <anna.daginova@yandex.ru>
2026-03-16 15:47:32 +0300

    готовая работа

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,59 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules, rules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules, [
+  rules.caseInsensitiveStringIncludes(),
+  rules.arrayAsRange(),
+]);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        const option = document.createElement("option");
+        option.value = name;
+        option.textContent = name;
+        return option;
+      }),
+    );
+  });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const parentElement = action.parentNode;
+      const inputElement = parentElement.querySelector("input");
+      if (inputElement) {
+        inputElement.value = "";
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        const fieldName = action.dataset.field;
+        state[fieldName] = "";
+      }
     }
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    const target = {};
+
+    if (state.date) {
+      target.date = state.date;
+    }
+
+    if (state.customer) {
+      target.customer = state.customer;
+    }
+
+    if (state.seller && state.seller !== "all") {
+      target.seller = state.seller;
+    }
+
+    const totalFrom = state.totalFrom ? Number(state.totalFrom) : undefined;
+    const totalTo = state.totalTo ? Number(state.totalTo) : undefined;
+
+    if (totalFrom !== undefined || totalTo !== undefined) {
+      target.total = [totalFrom, totalTo];
+    }
+
+    return data.filter((row) => compare(row, target));
+  };
 }
